229f432300000ab67f14b5a60bc9014027b68817
李進興 Vulcan <vulcan@miniasp.com>
2019-09-25 08:56:43 +0800

    第一次建立專案

---
+++ AsyncCourse/AsyncCourse/AsyncLib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AsyncCourse
+{
+    public class AsyncLib
+    {
+        /// <summary>
+        /// 按下任一按鍵以繼續
+        /// </summary>
+        public static void PressAnyKey()
+        {
+            Console.WriteLine("Press any key for continuing...");
+            Console.ReadKey();
+        }
+    }
+}
